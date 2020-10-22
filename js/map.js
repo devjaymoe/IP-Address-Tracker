41660a1e243d9dbdfe07d65366802772413e9443
Devin_Moore <d.j.moore209@gmail.com>
2020-10-22 17:16:47 -0400

    map is interactable, map updates with search input upon clicking submit button

--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
 let myIp, myLocation, myTimezone, myISP, mymap;
+let myIcon = L.icon({ iconUrl: "../images/icon-location.svg" });
 
 let domIP = document.querySelector(".ipAddr");
 let domLoc = document.querySelector(".location");
@@ -57,8 +58,6 @@ async function mapSetUp() {
         }
     ).addTo(myMap);
 
-    let myIcon = L.icon({ iconUrl: "../images/icon-location.svg" });
-
     let marker = L.marker(myLocation ? myLocation : [51.505, -0.09], {
         icon: myIcon,
     }).addTo(myMap);
@@ -66,10 +65,56 @@ async function mapSetUp() {
 
 mapSetUp();
 
+// myMap = L.map("mapid").setView(myLocation ? myLocation : [51.505, -0.09], 13);
+
+// L.tileLayer(
+//     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+//     {
+//         attribution:
+//             'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//         maxZoom: 18,
+//         id: "mapbox/streets-v11",
+//         tileSize: 512,
+//         zoomOffset: -1,
+//         accessToken:
+//             "pk.eyJ1IjoiZGV2aW5rIiwiYSI6ImNrZ2p0NndzNzAxdGEzMW12YnAzcThhaXMifQ.uZQwQmVt2jWHpmgGJJAr7A",
+//     }
+// ).addTo(myMap);
+
+// let myIcon = L.icon({ iconUrl: "../images/icon-location.svg" });
+
+// let marker = L.marker(myLocation ? myLocation : [51.505, -0.09], {
+//     icon: myIcon,
+// }).addTo(myMap);
+
 const input = document.getElementById("searchInput");
 const submit = document.getElementById("submitButton");
 
-submit.addEventListener("click", () => {
+submit.addEventListener("click", async () => {
     myIp = input.value;
-    console.log(myIp);
+
+    await axios
+        .get(
+            `https://geo.ipify.org/api/v1?apiKey=at_HYvw4DfeTgHyjOCtMsdHVvHwOXvre&ipAddress=${myIp}`
+        )
+        .then((response) => {
+            myLocation = [
+                response.data.location.lat,
+                response.data.location.lng,
+            ];
+            myISP = response.data.isp;
+            myTimezone = response.data.location.timezone;
+
+            domIP.textContent = myIp;
+            domLoc.textContent = `${response.data.location.city}, ${response.data.location.region} ${response.data.location.postalCode}`;
+            domTime.textContent = `UTC ${myTimezone}`;
+            domISP.textContent = myISP;
+        })
+        .catch((error) => console.log(error));
+
+    marker = L.marker(myLocation, {
+        icon: myIcon,
+    }).addTo(myMap);
+
+    myMap.setView(myLocation, 13);
 });
